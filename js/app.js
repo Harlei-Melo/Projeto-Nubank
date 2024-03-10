036ff897df1a490842a92a6a6549a88fd3aa340c
Harlei Melo <idzeronee@gmail.com>
2024-03-10 15:23:26 -0300

    Finalização da seção slider e algumas correções de espaçamenti

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 //Scroll header
 
-function scrollHeader(){
+function scrllHeader(){
     const nav = document.getElementById('header');
 
     if(this.scrollY >= 50) nav.classList.add('active-header'); else nav.classList.remove('active-header');
